a13157a4266017777d71fc36a3b5c5004f00c65b
Mateus Bessa <mateusbessacontato@gmail.com>
2023-11-04 16:46:56 -0300

    Registrando usuario no sistema

--- back-end/controllers/UserController.js
+++ back-end/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -42,6 +43,21 @@ module.exports = class UserController {
         if(userExists) {
             res.status(422).json({message: 'Email já cadastrado!'})
             return
+        }
+
+        //create a password --> preciso criptografar essa paradinha antes de subir no banco!
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        //create a user
+        //Quando o nome da variável é igual o valor da chave, eu posso passar apenas 1 deles. Veja a diferença do password pro resto!
+        const user = new User({name, email, phone, password: passwordHash})
+
+        try {
+            const newUser = await user.save()
+            res.status(201).json({message: 'Usuário criado!', newUser})
+        } catch (error) {
+            res.status(500).json({message: error})
         }
     }
 }
