3af770adcb0e3e9df14b2e7ed469fde338b0407e
Mateus Bessa <mateusbessacontato@gmail.com>
2023-11-04 17:58:17 -0300

    Funcao de login do sistema

--- back-end/controllers/UserController.js
+++ back-end/controllers/UserController.js
@@ -45,7 +45,7 @@ module.exports = class UserController {
             res.status(422).json({message: 'Email já cadastrado!'})
             return
         }
-
+        
         //create a password --> preciso criptografar essa paradinha antes de subir no banco!
         const salt = await bcrypt.genSalt(12)
         const passwordHash = await bcrypt.hash(password, salt)
@@ -61,4 +61,39 @@ module.exports = class UserController {
             res.status(500).json({message: error})
         }
     }
+
+    static async login(req, res) {
+        const {email, password} = req.body
+
+        if(!email) {
+            res.status(422).json({message: 'O email é obrigatório'})
+            return
+        }
+
+        if(!password) {
+            res.status(422).json({message: 'A senha é obrigatória'})
+            return
+        }
+
+        //Checando se o email é um email cadastrado
+        const user = await User.findOne({email: email})
+
+        if(!user) {
+            res.status(422).json({message: 'Não há usuário cadastrado com este email!'})
+            return
+        }
+
+        //check if password match with db passoword
+        //se o email for cadastrado ele não entra no if, e entra aqui, pegando a senha do usuário criptografada e comparando!
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        //Se "checkPassword" me retornar false, vai entrar aqui!
+        if(!checkPassword) {
+            res.status(422).json({message: 'Senha inválida!'})
+            return
+        }
+
+        await createUserToken(user, req, res)
+    }
+    
 }
